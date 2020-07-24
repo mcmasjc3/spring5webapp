aa98ff01e0615e6e14932e827178bee8da04ceaf
Jim Mcmaster <jim.mcmaster@ringcentral.com>
2020-07-24 16:59:01 -0600

    Author assignment

--- src/main/java/guru/springframework/spring5webapp/controllers/AuthorController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/AuthorController.java
@@ -7,20 +7,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class BookController {
+public class AuthorController {
 
-  private final BookRepository bookRepository;
-  private AuthorRepository authorRepository;
+  private final AuthorRepository authorRepository;
 
-  public BookController(BookRepository bookRepository, AuthorRepository authorRepository) {
-    this.bookRepository = bookRepository;
+  public AuthorController(AuthorRepository authorRepository) {
     this.authorRepository = authorRepository;
-  }
-
-  @RequestMapping("/books")
-  public String getBooks(Model model) {
-    model.addAttribute("books", bookRepository.findAll());
-    return "books/list";
   }
 
   @RequestMapping("/authors")
